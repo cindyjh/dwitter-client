41eb93665eaf489e688d2c95cfd1dd3d452f2f3b
joohee <xorjsqnfl@gmail.com>
2021-10-03 04:01:49 +0900

    Feat: front에서 로그인 만료 시(http status 401받을 시) authEventBus에 태워 로그인 페이지로 이동하도록 한다.

--- src/network/http.js
+++ src/network/http.js
@@ -1,6 +1,7 @@
 export default class HttpClient {
-    constructor(baseURL) {
+    constructor(baseURL, authErrorEventBus) {
         this.baseURL = baseURL
+        this.authErrorEventBus = authErrorEventBus
     }
 
     async fetch(url, options) {
@@ -25,7 +26,12 @@ export default class HttpClient {
             const message = data && data.message 
                 ? data.message
                 : 'Something went wrong!'
-            throw new Error(message)
+            const error = new Error(message)
+            if (res.status === 401) {
+                this.authErrorEventBus.notify(error)
+                return
+            }
+            throw error
         }
 
         return data
